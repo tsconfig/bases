9d7a8d2c913f9bab093c8e38adfbcc369bee16a9
Sora Morimoto <sora@morimoto.io>
2025-12-26 22:24:18 +0900

    chore: add `deno.json` and `deno.lock` for Deno v2
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- scripts/generate-recommend.ts
+++ scripts/generate-recommend.ts
@@ -1,24 +1,23 @@
-import stripJsonComments from "https://esm.sh/strip-json-comments";
-import * as bufio from "https://deno.land/std@0.164.0/io/buffer.ts";
-import * as path from "https://deno.land/std/path/mod.ts";
+import { parse as parseJsonc } from "@std/jsonc";
+import * as path from "@std/path";
 
 const tsconfigStorage = await Deno.makeTempDir({ prefix: "tsconfig" });
 
 // Generate a tsconfig
-const p = await Deno.run({ cmd: ["npx", "-p", "typescript", "tsc", "--init"], stdout: "piped", cwd: tsconfigStorage });
-for await (const line of bufio.readLines(p.stdout!)) {
-  console.warn(line);
-}
+const cmd = new Deno.Command("npx", { args: ["-p", "typescript", "tsc", "--init"], stdout: "piped", cwd: tsconfigStorage });
+const output = await cmd.output();
+if (!output.success) throw new Error("Failed to run tsc --init")
+console.warn(new TextDecoder().decode(output.stdout));
 
 let packageText = await Deno.readTextFile(path.join(tsconfigStorage, "tsconfig.json"));
 // This will strip comments
-const parsed = JSON.parse(stripJsonComments(packageText));
+const parsed = parseJsonc(packageText);
 
 // `display` field will be dropped at generating npm package, so prevent the order from being last in the JSON file
 parsed.display = "Recommended";
-parsed["$schema"] = "https://www.schemastore.org/tsconfig";
+parsed.$schema = "https://www.schemastore.org/tsconfig";
 
-const result = JSON.stringify(parsed, null, "  ");
+const result = JSON.stringify(parsed, null, 2);
 
 const npmResponse = await fetch(`https://unpkg.com/@tsconfig/svelte/tsconfig.json`);
 const npmPackage = await npmResponse.text();
